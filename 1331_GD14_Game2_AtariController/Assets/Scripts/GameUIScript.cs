857690aedff148855147429b8113fe67584f9d66
Yi Wang <s2533013@kpuenta.ca>
2026-06-02 16:08:47 -0700

    UITemplate in scene

---
+++ 1331_GD14_Game2_AtariController/Assets/Scripts/GameUIScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//potential way to update the timer and the score
+//Can just compile the script for the score/time tracker and the UI updater onto the same script/manager object
+public class GameUIScript : MonoBehaviour
+{
+    [SerializeField] private Image _timerFill;
+
+    private void UpdateTimeBar()
+    {
+        
+    }
+}
